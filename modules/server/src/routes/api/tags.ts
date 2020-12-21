042e58e776e20c7c3024fa2af6cefec2267d12df
hamboomger <hamboomger.dev@gmail.com>
2020-12-21 14:45:07 +0100

    feat(server-api): additional /tags routes added

--- modules/server/src/routes/api/tags.ts
+++ modules/server/src/routes/api/tags.ts
@@ -1,16 +1,31 @@
 import { Router } from 'express';
+import { validate } from 'express-validation';
 import Poem from '../../model/poem';
 import { getCurrentUser } from '../../lib/currentUser';
+import tagValidationSchema from './validation/tagValidationSchema';
+import { ITagNoRefs, Tag } from '../../model/tag';
 
 const route = Router();
 route.get('/api/tags', async (req, res) => {
   const user = getCurrentUser();
-  if (user !== undefined) {
-    const poems = await Poem.find({ user: user._id });
-    const allTags = new Set(poems.flatMap((poem) => poem.tags));
-    const tagsSorted = Array.from(allTags).sort();
-    res.json(tagsSorted);
-  }
+  const poems = await Poem.find({ user: user._id });
+  const allTags = new Set(poems.flatMap((poem) => poem.tags));
+  const tagsSorted = Array.from(allTags).sort();
+  res.json(tagsSorted);
+});
+route.put(
+  '/api/tags/update',
+  validate(tagValidationSchema),
+  async (req, res) => {
+    const tag: ITagNoRefs = req.body;
+    Tag.updateOne({ name: tag.name }, tag, { upsert: true });
+    res.sendStatus(200);
+  },
+);
+route.delete('/api/tags/:name/delete', async (req, res) => {
+  const { name } = req.query;
+  await Tag.deleteOne({ name: name as string });
+  res.sendStatus(200);
 });
 
 export { route as tagsRoute };
